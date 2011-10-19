414e60c24062619a795bf3a399c9e44fb82919bc
pauli.rikula <pauli.rikula@gmail.com>
2011-10-19 20:32:31 +0000

    an a-star implementation in Python see http://kks.cabal.fi/A-star

---
+++ a-star/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='Yaastar',
+      version='0.1',
+      description='Yet another A-star graph search algorithm implementation',
+      author='Pauli Rikula',
+      author_email='pauli.rikula@gmail.com',
+      url='http://kks.cabal.fi/A-star',
+      download_url = 'http://code.google.com/p/my-never-ending-projects/',
+      packages=['yaastar'],
+      license='See the source files'
+     )
+
