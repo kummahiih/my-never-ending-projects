8e8c07ab44dc69aff218ad9073907c841bd0ba18
pauli.rikula <pauli.rikula@gmail.com>
2012-12-30 23:10:57 +0000

    siirrelty tulokset omaan hakemistoon, muutama wav lisätty kuten myös wav -skripti

---
+++ laskuja/säädata/data_to_wav.py
@@ -0,0 +1,44 @@
+#for parsing
+from parse_data import get_data
+
+#for audio
+import wave
+import struct
+
+def scale( aFloatList ,amin, amax):
+    samples_max = max(aFloatList)
+    samples_min = min(aFloatList)
+
+    if samples_min - samples_max == 0:
+        return [ (amin + amax) / 2 for _ in aFloatList ]
+
+    returned = [
+        amin +
+        ((sample-samples_min) *
+                (amax - amin) / (samples_max - samples_min)
+         )
+
+        for sample in aFloatList ]
+
+    return returned
+
+
+def generate_wavs():
+    data = get_data()
+    temperature = [ r[1] for r in data]
+    scaled_temperature = scale(temperature,-32767,32767)
+
+    for framerate in range(1000,128000,5000):
+        noise_output = wave.open('results/temperature_%d.wav'%framerate, 'w')
+        noise_output.setparams((2, 2, framerate, 0, 'NONE', 'not compressed'))
+
+        for i in scaled_temperature:
+                value = int(i +0.5)
+                packed_value = struct.pack('h', value)
+                noise_output.writeframes(packed_value)
+                noise_output.writeframes(packed_value)
+
+        noise_output.close()
+
+
+generate_wavs()
