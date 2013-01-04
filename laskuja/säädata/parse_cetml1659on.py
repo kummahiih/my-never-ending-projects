e4d3a971edef45a7d5dc07e3902410b2300da75f
pauli.rikula <pauli.rikula@gmail.com>
2013-01-04 07:21:35 +0000

    about the source data

--- laskuja/säädata/parse_cetml1659on.py
+++ laskuja/säädata/parse_cetml1659on.py
@@ -1,4 +1,16 @@
+#data for this file is available at
 #http://www.metoffice.gov.uk/hadobs/hadcet/cetml1659on.dat
+
+##http://www.metoffice.gov.uk/hadobs/hadcet/terms_and_conditions.html :
+
+##HadCET Data: Terms and Conditions
+##HadCET is subject to Crown copyright protection. The material may be downloaded to file or printer for the purposes of private study and scientific research. Any other proposed use of the material is subject to a copyright licence available from the Met Office. Licences and further information can be obtained from the Met Office IPR Officer, FitzRoy Road, Exeter, EX1 3PB E-mail: ipr@metoffice.gov.uk.
+##For further information on Crown Copyright policy and licensing arrangements, see the guidance featured on HMSO's web site.
+##When publishing work using the data, please use the following citation:
+##Parker, D.E., T.P. Legg, and C.K. Folland. 1992. A new daily Central England Temperature Series, 1772-1991. Int. J. Clim., Vol 12, pp 317-342 
+##The source should also be quoted in the acknowledgements section as www.metoffice.gov.uk/hadobs.
+
+# I think this is private research and study ..
 
 import csv
 import re
