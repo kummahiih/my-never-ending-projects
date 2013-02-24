489829edd5d4c81067b050f885cc0ff186e9d258
pauli.rikula <pauli.rikula@gmail.com>
2013-02-24 11:29:28 +0000

    kommentteja

--- laskuja/säädata/plot_willab_data_airpressure.py
+++ laskuja/säädata/plot_willab_data_airpressure.py
@@ -9,8 +9,10 @@ if __name__ == "__main__":
     delta = datetime.timedelta(0,600,0)
     signal = get_data_airpressure()
     makeplot(signal, monthly, step = 100)
-    new_signal = list( takeOnlyValuesBetveen(
-        interpolateSignal(signal, delta),
-        961,1100)
+    
+    new_signal = list(
+        takeOnlyValuesBetveen( #this takes only values between 961 and 1100
+            interpolateSignal(signal, delta),  #this respamples data to 'delta' time intervals
+            961,1100)
         )
     makeplot(new_signal, monthly, step = 100)
